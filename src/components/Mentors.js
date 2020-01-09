1d6f40a9db6974d679c51f0568b66a73d4c7c4e6
Rishabh Dubey <rishabhdubey020@gmail.com>
2020-01-09 20:46:43 +0530

    added update mentor

--- src/components/Mentors.js
+++ src/components/Mentors.js
@@ -7,6 +7,15 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Button from '@material-ui/core/Button';
 import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
 
 const useStyles = makeStyles(theme => ({
   formControl: {
@@ -23,7 +32,20 @@ const useStyles = makeStyles(theme => ({
 function Mentors() {
   const classes = useStyles();
 
+  const [singleMentor, setSingleMentor] = useState();
   const [mentors, setMentors] = useState([]);
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = (e, m) => {
+    setFormData(m);
+    setSingleMentor(m);
+    console.log(`HERE: ${JSON.stringify(m)}`);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     getMentors();
@@ -52,6 +74,35 @@ function Mentors() {
     }
   };
 
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    mobile: '',
+    dateOfBirth: '',
+    gender: ''
+  });
+
+  const { name, email, mobile, dateOfBirth, gender } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const updateMentor = async id => {
+    try {
+      console.log(`ID: ${id}`);
+      const updated = await axios.post(`/api/mentor/${id}`, formData);
+      if (updated) {
+        console.log(`Updated ${updated.data.data.id}`);
+        setOpen(false);
+        return;
+      }
+      setOpen(false);
+      return;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   if (mentors) {
     return (
       <div>
@@ -64,7 +115,12 @@ function Mentors() {
             return (
               <ListItem divider key={mentor._id}>
                 <ListItemText primary={mentor.name} />
-                <Button variant='outlined' onClick={() => console.log(`HERE`)}>
+                <Button
+                  variant='outlined'
+                  onClick={e => {
+                    handleClickOpen(e, mentor);
+                  }}
+                >
                   Update
                 </Button>
                 <DeleteOutlinedIcon
@@ -75,6 +131,79 @@ function Mentors() {
             );
           })}
         </List>
+
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby='form-dialog-title'
+        >
+          <DialogTitle id='form-dialog-title'>Update</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              style={{ width: '96%' }}
+              label='Name'
+              variant='outlined'
+              name='name'
+              value={name}
+              onChange={e => onChange(e)}
+            />
+            <TextField
+              style={{ width: '96%' }}
+              label='Email'
+              variant='outlined'
+              name='email'
+              value={email}
+              onChange={e => onChange(e)}
+            />
+            <TextField
+              style={{ width: '96%' }}
+              label='Mobile'
+              variant='outlined'
+              name='mobile'
+              value={mobile}
+              onChange={e => onChange(e)}
+            />
+            <TextField
+              style={{ width: '96%' }}
+              label='Date Of Birth'
+              variant='outlined'
+              name='dateOfBirth'
+              value={dateOfBirth}
+              onChange={e => onChange(e)}
+            />
+            <FormControl variant='outlined' className={classes.formControl}>
+              <InputLabel
+                // ref={inputLabel}
+                id='demo-simple-select-outlined-label'
+              >
+                Gender
+              </InputLabel>
+              <Select
+                labelId='demo-simple-select-outlined-label'
+                // labelWidth={labelWidth}
+                name='gender'
+                value={gender}
+                onChange={e => onChange(e)}
+              >
+                <MenuItem value={'male'}>Male</MenuItem>
+                <MenuItem value={'female'}>Female</MenuItem>
+              </Select>
+            </FormControl>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color='primary'>
+              Cancel
+            </Button>
+            <Button
+              variant='outlined'
+              color='primary'
+              onClick={() => updateMentor(singleMentor._id)}
+            >
+              Update
+            </Button>
+          </DialogActions>
+        </Dialog>
       </div>
     );
   }
